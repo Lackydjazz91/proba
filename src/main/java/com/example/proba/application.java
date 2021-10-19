8db36c102ffe458160e7b7a9fdc30b568fb15f0d
Aleksey Aleksin <lackydjazz91@rambler.ru>
2021-10-19 14:26:49 +0300

    new line

---
+++ src/main/java/com/example/proba/application.java
@@ -0,0 +1,12 @@
+package com.example.proba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(application.class, args);
+    }
+}
---
+++ src/main/java/com/example/proba/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.proba.repos;
+
+import com.example.proba.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
